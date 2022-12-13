f3007f95a3cce940c0ad8a8d79b069524f928fce
zhan-yao <3119954407@qq.com>
2022-12-13 10:53:32 +0800

    oss上传测试完成

--- gulimall-product/src/test/java/club/zhanshen/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/club/zhanshen/gulimall/product/GulimallProductApplicationTests.java
@@ -1,21 +1,18 @@
 package club.zhanshen.gulimall.product;
 
-import club.zhanshen.gulimall.product.entity.BrandEntity;
 import club.zhanshen.gulimall.product.service.BrandService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GulimallProductApplicationTests {
+public class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
     @Test
-    void contextLoads() {
-        BrandEntity brandEntity = new BrandEntity();
-        brandEntity.setName("名字");
-        brandService.save(brandEntity);
+    public void test(){
     }
 
 }
--- gulimall-ware/src/test/java/club/zhanshen/gulimall/ware/GulimallWareApplicationTests.java
+++ gulimall-ware/src/test/java/club/zhanshen/gulimall/ware/GulimallWareApplicationTests.java
@@ -1,6 +1,6 @@
 package club.zhanshen.gulimall.ware;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
